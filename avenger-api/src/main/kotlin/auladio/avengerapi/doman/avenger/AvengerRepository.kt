f7d6db0bbc5022727c2b36b9ea2df70f72283c7b
Wellington-wsn <fogosark@hotmail.com>
2024-04-21 14:35:53 -0300

    created docker resources

--- avenger-api/src/main/kotlin/auladio/avengerapi/doman/avenger/AvengerRepository.kt
+++ avenger-api/src/main/kotlin/auladio/avengerapi/doman/avenger/AvengerRepository.kt
@@ -3,7 +3,7 @@ package auladio.avengerapi.doman.avenger
 
 interface AvengerRepository {   // Interface de acesso, devolve o objeto
     fun getDetail(id: Long): Avenger // Função GET
-    fun getAvengers(): kotlin.collections.List<Avenger>
+    fun getAvengers(): List<Avenger>
     fun create(avenger: Avenger): Avenger
     fun delete(id: Long)
     fun update(avenger: Avenger): Avenger
